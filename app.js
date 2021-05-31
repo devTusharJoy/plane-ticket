098394b3268bfa286f6283742345f90855e4662e
JPromee <tusharahamedjoyz@gmail.com>
2021-06-01 01:13:26 +0600

    price-count done

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-function handleTicketNumber(ticket, increase){
-    let ticketInput = document.getElementById(ticket + '-ticket');
-    let ticketCount = parseInt(ticketInput.value);
+// increase and decrease amount
 
-    let newCount = ticketCount;
+function handleTicketNumber(ticket, increase){
+    let ticketCount = getInputValue(ticket);
+
+    let newTicket = ticketCount;
     if(increase == true){
-        newCount = ticketCount + 1;
+        newTicket = ticketCount + 1;
     }
 
     if(increase == false  && ticketCount > 0){
-        newCount = ticketCount - 1;
+        newTicket = ticketCount - 1;
     }
 
-    ticketInput.value = newCount;
+    document.getElementById(ticket + '-ticket').value = newTicket;
+    
+    totalCost();
 
+}
+
+// total cost
+
+function totalCost(){
+    let firstClassTicket = getInputValue('firstClass');
+    let economyClassTicket = getInputValue('economyClass');
+
+    let totalPrice = firstClassTicket * 150 + economyClassTicket * 100;
+    document.getElementById('sub-Total').innerText = totalPrice;
+
+    let tax = totalPrice * 0.1;
+    document.getElementById('tax').innerText = tax;
+
+    let totalAmount = totalPrice + tax;
+    document.getElementById('total-Amount').innerText = totalAmount;
+}
+
+function getInputValue(ticket){
+    let ticketInput = document.getElementById(ticket + '-ticket');
+    let ticketCount = parseInt(ticketInput.value);
+
+    return ticketCount;
 }
 
